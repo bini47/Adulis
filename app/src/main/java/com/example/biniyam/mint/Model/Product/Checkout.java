85808c8e02b10e5382e9084d3a3abce971b44b2e
voodoo <nohashanon@gmail.com>
2019-05-09 14:01:12 +0300

    stoke logic added

---
+++ app/src/main/java/com/example/biniyam/mint/Model/Product/Checkout.java
@@ -0,0 +1,16 @@
+package com.example.biniyam.mint.Model.Product;
+
+public class Checkout {
+
+    String firstname;
+    String lastname;
+    String companyname;
+    String city;
+    String streetname;
+    String housenumber;
+    String phone;
+    String email;
+    String paymentmethod;
+    String note;
+    String transactionmethod;
+}
